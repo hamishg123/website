a4532d4efe098ac55d65c367233f8fee9a99413c
hamishg123 <hamishgordon051@gmail.com>
2024-11-06 11:31:32 +0000

    Update file.js

--- file.js
+++ file.js
@@ -1,5 +1,5 @@
-const divElement = document.querySelector('div'); // Replace 'div' with the ID or class of your actual div element
+const divElement = document.querySelector('game'); // Replace 'div' with the ID or class of your actual div element
 
 divElement.addEventListener('click', function() {
-  window.location.href = 'https://www.example.com'; // Replace 'https://www.example.com' with the URL of your desired page
-});
+  window.location.href = 'https://hamish.site/main.html'; // Replace 'https://www.example.com' with the URL of your desired page
+});e
