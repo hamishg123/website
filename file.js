4af5237732ed5ef0c823714aa1578b74a2e67db8
hamishg123 <hamishgordon051@gmail.com>
2024-11-06 11:23:07 +0000

    Create file.js

---
+++ file.js
@@ -0,0 +1,5 @@
+const divElement = document.querySelector('div'); // Replace 'div' with the ID or class of your actual div element
+
+divElement.addEventListener('click', function() {
+  window.location.href = 'https://www.example.com'; // Replace 'https://www.example.com' with the URL of your desired page
+});
