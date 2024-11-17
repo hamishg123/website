f7af7b06d8d97e6d08db1ec6fb7a17b2d41fc60a
hamishg123 <hamishgordon051@gmail.com>
2024-11-17 13:30:49 +0000

    Update file.js

--- file.js
+++ file.js
@@ -1,6 +1,7 @@
 let email;
+
 document.getElementById("subscribe").onclick = function(){
-  email = document.getElementById("input").value;
-  console.log(email);
+    email = document.getElementById("input").value;
+    console.log(email);
 }
 
